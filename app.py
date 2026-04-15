f3a9540acc200a19be3dcc74aa206d911a460731
pavanthallapalli <pavanthallapalli1@gmail.com>
2026-04-15 22:55:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 import streamlit as st
 import numpy as np
-import joblib
+import pickle
 
 # Load model & scaler
-model = joblib.load("model.pkl")
-scaler = joblib.load("scaler.pkl")
+model = pickle.load(open("model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
 
 st.title("Student Performance Prediction App")
 st.write("Enter student details to predict performance category")
 
-# Inputs
 hours = st.slider("Hours Studied", 0, 12, 5)
 previous = st.slider("Previous Scores", 0, 100, 50)
 extra = st.selectbox("Extracurricular Activities", ["Yes", "No"])
 sleep = st.slider("Sleep Hours", 0, 12, 6)
 papers = st.slider("Sample Papers Practiced", 0, 10, 3)
 
-# Convert Yes/No
 extra = 1 if extra == "Yes" else 0
 
-# Prediction
 if st.button("Predict"):
     try:
         input_data = np.array([[hours, previous, extra, sleep, papers]])
@@ -37,3 +34,4 @@ if st.button("Predict"):
 
     except Exception as e:
         st.error(str(e))
+
